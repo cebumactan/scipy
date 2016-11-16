ab3640f9f041fadd430356e539c3f896a16ad9fb
mingi.lee <leemdropboxhome@gmail.com>
2016-11-16 22:53:52 +0300

    datainput

--- chardiff/chardiff.py
+++ chardiff/chardiff.py
@@ -4,4 +4,18 @@ Created on Wed Nov 16 18:46:22 2016
 
 @author: leem
 """
+import numpy as np
+import matplotlib.pyplot as plt
 
+# import data
+mydata = np.load('./mydata.npz')
+Hplus = mydata['arr_0']
+Voltage = mydata['arr_2']
+t = mydata['arr_3']
+pH = mydata['arr_4']
+
+figure(1)
+plt.plot(t,pH)
+
+figure(2)
+plt.plot(t,Hplus)
