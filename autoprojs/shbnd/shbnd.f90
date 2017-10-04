5226111dde791dd1b7d585a1d4cfe849da40f5a2
leem <leemdropboxhome@gmail.com>
2017-10-04 18:54:26 +0300

    eigenvector added but commented

--- autoprojs/shbnd/shbnd.f90
+++ autoprojs/shbnd/shbnd.f90
@@ -31,6 +31,7 @@
 !           PAR(24): 
 !           PAR(25): c1 coefficient 
 !           PAR(26): 
+
 !           PAR(27): 
 !           PAR(28): 
 !           PAR(29): 
@@ -105,17 +106,19 @@
 !      DOUBLE PRECISION V0(NDM),V1(NDM),G0(NDM),G1(NDM)
       DOUBLE PRECISION MAT0(NDM,NDM),MAT1(NDM,NDM)
 
-      DOUBLE PRECISION EPS0, EPS1, A, M, N, L, D, a, b, P, Q, R, R0, R1
+      DOUBLE PRECISION EPS0, EPS1, A, M, N, L, D, a, b, R0, R1
       DOUBLE PRECISION mu01, mu02, mu03, mu11, mu12
+      DOUBLE PRECISION X01(NDM), X01(NDM), X01(NDM), X01(NDM)
+      DOUBLE PRECISION c01, c02, c03, c11, c12
 
-
-	EPS0=PAR(5)
-	EPS1=PAR(6)
 
         A=PAR(1)
         M=PAR(2)
 	N=PAR(3)
 	L=PAR(4)
+
+	EPS0=PAR(5)
+	EPS1=PAR(6)
 		
 	D=1. + 2*A - M - N
 	a=(2.+2.*A-N)/D + 2.*(1.+A)*L/D
@@ -156,6 +159,27 @@
 	mu12 = -1.
 
 ! let AUTO compute the eigenvectors
+!	FB(1)= MAT0(1,1)*X01(1) + MAT0(1,2)*X01(2) + MAT0(1,3)*X01(3)- mu01*X01(1)
+!	FB(2)= MAT0(2,1)*X01(1) + MAT0(2,2)*X01(2) + MAT0(2,3)*X01(3)- mu01*X01(2)
+!	FB(3)= MAT0(3,1)*X01(1) + MAT0(3,2)*X01(2) + MAT0(3,3)*X01(3)- mu01*X01(3)
+!
+!	FB(4)= MAT0(1,1)*X02(1) + MAT0(1,2)*X02(2) + MAT0(1,3)*X03(3)- mu01*X02(1)
+!	FB(5)= MAT0(2,1)*X02(1) + MAT0(2,2)*X02(2) + MAT0(2,3)*X03(3)- mu01*X02(2)
+!	FB(6)= MAT0(3,1)*X02(1) + MAT0(3,2)*X02(2) + MAT0(3,3)*X03(3)- mu01*X02(3)
+!
+!	FB(7)= MAT0(1,1)*X03(1) + MAT0(1,2)*X03(2) + MAT0(1,3)*X03(3)- mu01*X03(1)
+!	FB(8)= MAT0(2,1)*X03(1) + MAT0(2,2)*X03(2) + MAT0(2,3)*X03(3)- mu01*X03(2)
+!	FB(9)= MAT0(3,1)*X03(1) + MAT0(3,2)*X03(2) + MAT0(3,3)*X03(3)- mu01*X03(3)
+!
+!	FB(10)= MAT0(1,1)*X11(1) + MAT0(1,2)*X11(2) + MAT0(1,3)*X11(3)- mu01*X11(1)
+!	FB(11)= MAT0(2,1)*X11(1) + MAT0(2,2)*X11(2) + MAT0(2,3)*X11(3)- mu01*X11(2)
+!	FB(12)= MAT0(3,1)*X11(1) + MAT0(3,2)*X11(2) + MAT0(3,3)*X11(3)- mu01*X11(3)
+!
+!	FB(13)= MAT0(1,1)*X12(1) + MAT0(1,2)*X12(2) + MAT0(1,3)*X12(3)- mu01*X12(1)
+!	FB(14)= MAT0(2,1)*X12(1) + MAT0(2,2)*X12(2) + MAT0(2,3)*X12(3)- mu01*X12(2)
+!	FB(15)= MAT0(3,1)*X12(1) + MAT0(3,2)*X12(2) + MAT0(3,3)*X12(3)- mu01*X12(3)
+
+! let AUTO compute the eigenvectors
 	FB(1)= MAT0(1,1)*PAR(7) + MAT0(1,2)*PAR(8) + MAT0(1,3)*PAR(9)- mu01*PAR(7)
 	FB(2)= MAT0(2,1)*PAR(7) + MAT0(2,2)*PAR(8) + MAT0(2,3)*PAR(9)- mu01*PAR(8)
 	FB(3)= MAT0(3,1)*PAR(7) + MAT0(3,2)*PAR(8) + MAT0(3,3)*PAR(9)- mu01*PAR(9)
@@ -177,20 +201,36 @@
 	FB(15)= MAT1(3,1)*PAR(19) + MAT1(3,2)*PAR(20) + MAT1(3,3)*PAR(21)- mu12*PAR(21)
 
 ! unit length eigenvectors
-	FB(22)= PAR(7)**2  + PAR(8)**2  + PAR(9)**2  -1
-	FB(23)= PAR(10)**2 + PAR(11)**2 + PAR(12)**2 -1
-	FB(24)= PAR(13)**2 + PAR(14)**2 + PAR(15)**2 -1
-	FB(25)= PAR(16)**2 + PAR(17)**2 + PAR(18)**2 -1
-	FB(26)= PAR(19)**2 + PAR(20)**2 + PAR(21)**2 -1
+	FB(16)= PAR(7)**2  + PAR(8)**2  + PAR(9)**2  -1
+	FB(17)= PAR(10)**2 + PAR(11)**2 + PAR(12)**2 -1
+	FB(18)= PAR(13)**2 + PAR(14)**2 + PAR(15)**2 -1
+	FB(19)= PAR(16)**2 + PAR(17)**2 + PAR(18)**2 -1
+	FB(20)= PAR(19)**2 + PAR(20)**2 + PAR(21)**2 -1
 
 ! unit length coefficients
-	FB(25)= PAR(22)**2 + PAR(23)**2 + PAR(24)**2 -1
-	FB(26)= PAR(25)**2 + PAR(26)**2 + PAR(27)**2 -1
+	FB(21)= PAR(22)**2 + PAR(23)**2 + PAR(24)**2 -1
+	FB(22)= PAR(25)**2 + PAR(26)**2 -1
+
+! boundary values
+	FB(23)= U0(1) - EPS0*(  PAR(22)*PAR(7) + PAR(23)*PAR(10) + PAR(24)*PAR(13)   ) 
+	FB(24)= U0(2) - EPS0*(  PAR(22)*PAR(8) + PAR(23)*PAR(11) + PAR(24)*PAR(14)   ) 
+	FB(25)= U0(3) - EPS0*(  PAR(22)*PAR(9) + PAR(23)*PAR(12) + PAR(24)*PAR(15)   ) 
+
+	FB(26)= U1(1) - EPS1*(  PAR(25)*PAR(16) + PAR(26)*PAR(19)   ) 
+	FB(27)= U1(2) - EPS1*(  PAR(25)*PAR(17) + PAR(26)*PAR(20)   ) 
+	FB(28)= U1(3) - EPS1*(  PAR(25)*PAR(18) + PAR(26)*PAR(21)   ) 
 
 
       END SUBROUTINE BCND
 
+      SUBROUTINE ICND
+      END SUBROUTINE ICND
 
+      SUBROUTINE FOPT 
+      END SUBROUTINE FOPT
+
+      SUBROUTINE PVLS
+      END SUBROUTINE PVLS
 
 
 
