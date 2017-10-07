c0188819fc79f1498178e7b3fed817a55c77a9da
leem <leemdropboxhome@gmail.com>
2017-10-07 20:11:01 +0300

    explicit solution formula but r(t)

--- autoprojs/shbnd/shbnd.f90
+++ autoprojs/shbnd/shbnd.f90
@@ -32,11 +32,12 @@
 !           PAR(25): c1 coefficient 
 !           PAR(26): 
 
-!           PAR(27): 
-!           PAR(28): 
-!           PAR(29): 
-!           PAR(31): 
-!           PAR(32): 
+!           PAR(27): PERIOD
+!           PAR(28): mu01
+!           PAR(29): mu02
+!           PAR(30): mu03
+!           PAR(31): mu11
+!           PAR(32): mu12
 !
 !-----------------------------------------------------------------------
 !-----------------------------------------------------------------------
@@ -78,19 +79,235 @@
 	N=PAR(3)
 	L=PAR(4)
 		
-	D=1. + 2*A - M - N
-	a=(2.+2.*A-N)/D + 2.*(1.+A)*L/D
-	b=(1.+m)    /D + (1.+M+N)*L/D
+	D=1.D0 + 2.D0*A - M - N
+	a=(2.D0+2.D0*A-N)/D + 2.D0*(1.D0+A)*L/D
+	b=(1.D0+m)    /D + (1.D0+M+N)*L/D
 
         P=U(1)
 	Q=U(2)
 	R=U(3)
 
-        F(1)= P * ( (R-a)/L + 2. - L*P*R - Q  )
-        F(2)= Q * (  1. - L*P*R - Q  ) + b*P*R
-        F(3)= R * ( (R-a)*(A-M-N)/(L*(1.+A)) + L*P*R + Q  ) / N
+        F(1)= P * ( (R-a)/L + 2.D0 - L*P*R - Q  )
+        F(2)= Q * (  1.D0 - L*P*R - Q  ) + b*P*R
+        F(3)= R * ( (R-a)*(A-M-N)/(L*(1.D0+A)) + L*P*R + Q  ) / N
 
       END SUBROUTINE FFFF
+
+
+      SUBROUTINE STPNT(NDIM,U,PAR,T)
+!     ---------- -----
+
+      IMPLICIT NONE
+      INTEGER, INTENT(IN) :: NDIM
+      DOUBLE PRECISION, INTENT(INOUT) :: U(NDIM),PAR(*)
+      DOUBLE PRECISION, INTENT(IN) :: T
+
+      DOUBLE PRECISION PERIOD, X, EPS0, EPS1
+      DOUBLE PRECISION A, M, N, L, D, a, b, P, Q, R
+      DOUBLE PRECISION R0, R1, S0, S1, mu01, mu02, mu03, mu11, mu12, DUMMY
+      DOUBLE PRECISION X01(NDM), X02(NDM), X03(NDM), X11(NDM), X12(NDM)
+      DOUBLE PRECISION C1, P00, Q00, R00, P11, Q11, R11, XEND
+
+! T runs in [0,1]			: SCALED VAR
+! X runs in [-PERIOD/2, PERIOD/2]	: ORIGINAL VAR
+! i.e., X = PERIOD(T-1/2)
+
+
+! INITIALIZE WHEN FIRST CALL
+     IF(T==0)THEN
+
+! Try Interval Length 100
+        PERIOD=100.D0
+
+        A=0.D0
+        M=-0.5D0
+	N=0.25D0
+	L=0.25D0
+		
+	D=1.D0 + 2.D0*A - M - N
+	a=(2.D0+2.D0*A-N)/D + 2.D0*(1.D0+A)*L/D
+	b=(1.D0+m)    /D + (1.D0+M+N)*L/D
+
+! Equilibrium points
+	R0 = a
+	R1 = R0 - (1.D0+A)*L/(A-M-N)
+	S0 = (1+M+N)/(1+A) - N/(R0*(1+A))
+	S1 = (1+M+N)/(1+A) - N/(R1*(1+A))
+
+! positive eigenvalue at M0
+	mu01 = 2.D0
+	mu02 = 1.D0
+	mu03 = -(M+N)*a/(N*L)
+
+! negative eigenvalue at M1
+	mu11 = -(1.D0+M+N)/(A-M-N)
+	mu12 = -1.D0
+
+! Explicit solution tangent to X02 at M0
+	EPS0 = 1.D-4
+	P00 = 0.D0 + EPS0*1.D0*X02(1)
+	Q00 = 0.D0 + EPS0*1.D0*X02(2)
+	R00 = a + EPS0*1.D0*X02(3)
+	C1 = (1-Q00)/Q00
+
+! Explicit solution tangent to X12 at M1
+	XEND = PERIOD/2.D0
+	P11 = 0.D0
+	Q11 = 1.D0/( 1.D0 + C1*DEXP(-XEND) )
+	R11 = ########
+	EPS1 = SQRT( (P11-0.D0)**2 + (Q11-1.D0)**2 + (R11-R1)**2 )
+
+! Provide the eigenvectors with unit length
+	 DUMMY = ( (1.D0-S0)/L ) * ( (1.D0+A)*R0/L + mu01/S0 ) - (N/R0)*( 1.D0/L + mu01 )*( R0/L + mu01/S0 )
+	 X01(1)=1.D0
+	 X01(2)=b*R0
+	 X01(3)=-(L+b)*R0* ( (1.D0+A)*R0/L + mu01/S0  ) / DUMMY
+         DUMMY = SQRT( X01(1)**2 + X01(2)**2 + X01(3)**2 )
+	 X01(1)=X01(1)/DUMMY
+	 X01(2)=X01(2)/DUMMY
+	 X01(3)=X01(3)/DUMMY
+
+	 DUMMY = ( (1.D0-S0)/L ) * ( (1.D0+A)*R0/L + mu02/S0 ) - (N/R0)*( 1.D0/L + mu02 )*( R0/L + mu02/S0 )
+	 X02(1)=0.D0
+	 X02(2)=1.D0
+	 X02(3)=-( (1.D0+A)*R0/L + mu02/S0 )/DUMMY
+         DUMMY = SQRT( X02(1)**2 + X02(2)**2 + X02(3)**2 )
+	 X02(1)=X02(1)/DUMMY
+	 X02(2)=X02(2)/DUMMY
+	 X02(3)=X02(3)/DUMMY
+
+	 X03(1)=0.D0
+	 X03(2)=0.D0
+	 X03(3)=1.D0
+
+	 DUMMY = ( (1.D0-S1)/L ) * ( (1.D0+A)*R1/L + mu11/s1 ) - (N/R1)*( 1.D0/L + mu11 )*( R1/L + mu11/S1 )
+	 X11(1)=1.D0
+	 X11(2)=(B-L)*R1/(1.D0+mu11)
+	 X11(3)=-( L*R1 + X11(2) ) * ( (1.D0+A)*R1/L + mu11/S1 ) / DUMMY
+         DUMMY = SQRT( X11(1)**2 + X11(2)**2 + X11(3)**2 )
+	 X11(1)=X11(1)/DUMMY
+	 X11(2)=X11(2)/DUMMY
+	 X11(3)=X11(3)/DUMMY
+
+	 DUMMY = ( (1.D0-S1)/L ) * ( (1.D0+A)*R1/L + mu12/s1 ) - (N/R1)*( 1.D0/L + mu12 )*( R1/L + mu12/S1 )
+	 X12(1)=0.D0
+	 X12(2)=1.D0
+	 X12(3)=- ( (1.D0+A)*R1/L + mu12/S1 ) / DUMMY
+         DUMMY = SQRT( X12(1)**2 + X12(2)**2 + X12(3)**2 )
+	 X12(1)=X12(1)/DUMMY
+	 X12(2)=X12(2)/DUMMY
+	 X12(3)=X12(3)/DUMMY
+
+!           PAR(1) : A = alpha
+!           PAR(2) : M = m
+!           PAR(3) : N = n
+!           PAR(4) : L = lambda
+!           PAR(5) : EPS0
+!           PAR(6) : EPS1
+!           PAR(7) : X01
+!           PAR(8) : 
+!           PAR(9) : 
+!           PAR(10): X02
+!           PAR(11): 
+!           PAR(12): 
+!           PAR(13): X03
+!           PAR(14): 
+!           PAR(15): 
+!           PAR(16): X11
+!           PAR(17): 
+!           PAR(18): 
+!           PAR(19): X12
+!           PAR(20): 
+!           PAR(21): 
+!           PAR(22): c0 coefficient 
+!           PAR(23): 
+!           PAR(24): 
+!           PAR(25): c1 coefficient 
+!           PAR(26): 
+
+!           PAR(27): PERIOD
+!           PAR(28): mu01
+!           PAR(29): mu02
+!           PAR(30): mu03
+!           PAR(31): mu11
+!           PAR(32): mu12
+
+
+         PAR(1) = A
+         PAR(2) = M
+         PAR(3) = N
+         PAR(4) = L
+         PAR(5) = EPS0
+         PAR(6) = EPS1
+         PAR(7) = X01(1) 
+         PAR(8) = X01(2)
+         PAR(9) = X01(3)
+
+         PAR(10)= X02(1)
+         PAR(11)= X02(2)
+         PAR(12)= X02(3)
+
+         PAR(13)= X03(1)
+         PAR(14)= X03(2)
+         PAR(15)= X03(3)
+
+         PAR(16)= X11(1)
+         PAR(17)= X11(2)
+         PAR(18)= X11(3)
+
+         PAR(19)= X12(1)
+         PAR(20)= X12(2)
+         PAR(21)= X12(3)
+
+         PAR(22)= 0.D0
+         PAR(23)= 1.D0
+         PAR(24)= 0.D0
+
+         PAR(25)= 0.D0
+         PAR(26)= -1.D0
+
+         PAR(27)= PERIOD
+         PAR(28)= mu01
+         PAR(29)= mu02
+         PAR(30)= mu03
+         PAR(31)= mu11
+         PAR(32)= mu12
+
+       ENDIF
+
+! Retrieve PARAMETER VALUES
+
+	PERIOD=PAR(27)
+        A=PAR(1)
+        M=PAR(2)
+	N=PAR(3)
+	L=PAR(4)
+
+	EPS0=PAR(5)
+	X02(1)=PAR(10)
+	X02(2)=PAR(11)
+	X02(3)=PAR(12)
+	Q00 = 0.D0 + EPS0*1.D0*X02(2)
+	C1 = (1-Q00)/Q00
+
+	D=1.D0 + 2.D0*A - M - N
+	a=(2.D0+2.D0*A-N)/D + 2.D0*(1.D0+A)*L/D
+	b=(1.D0+m)    /D + (1.D0+M+N)*L/D
+
+! Specify exact solution as starting point :
+	X=PERIOD*(T-0.5)
+	P = 0.D0
+	Q = 1.D0/( 1 + C1*DEXP(-X) )
+	R = #############
+
+	U(1)= P
+	U(2)= Q
+	U(3)= R
+	
+
+      END SUBROUTINE STPNT
+
+
 
       SUBROUTINE BCND(NDIM,PAR,ICP,NBC,U0,U1,FB,IJAC,DBC)
 !     ---------- ----
@@ -108,8 +325,8 @@
 
       DOUBLE PRECISION EPS0, EPS1, A, M, N, L, D, a, b, R0, R1
       DOUBLE PRECISION mu01, mu02, mu03, mu11, mu12
-      DOUBLE PRECISION X01(NDM), X01(NDM), X01(NDM), X01(NDM)
-      DOUBLE PRECISION c01, c02, c03, c11, c12
+!      DOUBLE PRECISION X01(NDM), X01(NDM), X01(NDM), X01(NDM)
+!      DOUBLE PRECISION c01, c02, c03, c11, c12
 
 
         A=PAR(1)
@@ -120,43 +337,43 @@
 	EPS0=PAR(5)
 	EPS1=PAR(6)
 		
-	D=1. + 2*A - M - N
-	a=(2.+2.*A-N)/D + 2.*(1.+A)*L/D
-	b=(1.+m)    /D + (1.+M+N)*L/D
+	D=1.D0 + 2.D0*A - M - N
+	a=(2.D0+2.D0*A-N)/D + 2.D0*(1.D0+A)*L/D
+	b=(1.D0+m)    /D + (1.D0+M+N)*L/D
 
 	R0 = a
-	R1 = R0 - (1.+A)*L/(A-M-N)
+	R1 = R0 - (1.D0+A)*L/(A-M-N)
 
 ! Linearized Matrix at M0
-	MAT0(1,1) = 2.
-	MAT0(1,2) = 0.
-	MAT0(1,3) = 0.
+	MAT0(1,1) = 2.D0
+	MAT0(1,2) = 0.D0
+	MAT0(1,3) = 0.D0
 	MAT0(2,1) = b*R0
-	MAT0(2,2) = 1.
-	MAT0(2,3) = 0.
+	MAT0(2,2) = 1.D0
+	MAT0(2,3) = 0.D0
 	MAT0(3,1) = (R0*L)*R0/N
 	MAT0(3,2) = R0/N
-	MAT0(3,3) = (  (A-M-N)/(L*(1+A)) - N*A/( L*(1+A)*R0 )   )*R0/N
+	MAT0(3,3) = (  (A-M-N)/(L*(1.D0+A)) - N*A/( L*(1.D0+A)*R0 )   )*R0/N
 
 ! Linearized Matrix at M1
-	MAT1(1,1) = -(1+M+N)/(A-M-N)
-	MAT1(1,2) = 0.
-	MAT1(1,3) = 0.
+	MAT1(1,1) = -(1.D0+M+N)/(A-M-N)
+	MAT1(1,2) = 0.D0
+	MAT1(1,3) = 0.D0
 	MAT1(2,1) = (b-L)*R1
-	MAT1(2,2) = -1.
-	MAT1(2,3) = 0.
+	MAT1(2,2) = -1.D0
+	MAT1(2,3) = 0.D0
 	MAT1(3,1) = (R1*L)*R1/N
 	MAT1(3,2) = R1/N
-	MAT1(3,3) = (  (A-M-N)/(L*(1+A)) - N*A/( L*(1+A)*R1 )   )*R1/N
+	MAT1(3,3) = (  (A-M-N)/(L*(1.D0+A)) - N*A/( L*(1.D0+A)*R1 )   )*R1/N
 
 ! positive eigenvalue at M0
-	mu01 = 2.
-	mu02 = 1.
+	mu01 = 2.D0
+	mu02 = 1.D0
 	mu03 = -(M+N)*a/(N*L)
 
 ! negative eigenvalue at M1
-	mu11 = -(1.+M+N)/(A-M-N)
-	mu12 = -1.
+	mu11 = -(1.D0+M+N)/(A-M-N)
+	mu12 = -1.D0
 
 ! let AUTO compute the eigenvectors
 !	FB(1)= MAT0(1,1)*X01(1) + MAT0(1,2)*X01(2) + MAT0(1,3)*X01(3)- mu01*X01(1)
